c1c4ea143a1782bafed30c836d246a0e50117ed6
Rudy Escobar <67067784+rescobar9@users.noreply.github.com>
2023-01-15 19:30:58 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(3)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+})
+radio.setGroup(1)
